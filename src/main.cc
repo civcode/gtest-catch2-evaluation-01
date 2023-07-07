1d2671cb706a4e0ffab4311b7874746a052389e5
chris <ivi3001@yahoo.de>
2023-07-07 16:59:05 +0200

    added gtest

--- src/main.cc
+++ src/main.cc
@@ -1,19 +1,28 @@
 #include "my_class.h"
 
 #include <iostream>
+#include <memory>
 
 using std::cout;
 using std::endl;
 
 int main() {
 
-  MyClass my_class;
+  //MyClass my_class;
 
-  for (int i=0; i<10; i++) {
-    cout << "fact(" << i << ") = " << my_class.FactorialRecursive(i) << endl;
-    cout << "fact(" << i << ") = " << my_class.FactorialIterative(i) << endl;
+  auto my_class_ptr = std::shared_ptr<MyClass>(new MyClass());
+
+  // for (int i=0; i<4; i++) {
+  //   cout << "fact(" << i << ") = " << my_class.FactorialRecursive(i) << endl;
+  //   cout << "fact(" << i << ") = " << my_class.FactorialIterative(i) << endl;
+  //   cout << endl;
+  // }
+
+  cout << "pointer" << endl;
+  for (int i=0; i<4; i++) {
+    cout << "fact(" << i << ") = " << my_class_ptr->FactorialRecursive(i) << endl;
+    cout << "fact(" << i << ") = " << my_class_ptr->FactorialIterative(i) << endl;
     cout << endl;
   }
-
   return 0;
 }
---
+++ tests/tests.cc
@@ -0,0 +1,30 @@
+#include "my_class.h"
+
+#include <gtest/gtest.h>
+
+#include <memory>
+
+//MyClass* my_class;
+
+std::shared_ptr<MyClass> my_class_ptr;
+
+// Tests factorial of 0.
+TEST(FactorialTest, HandlesZeroInput) {
+  EXPECT_EQ(my_class_ptr->FactorialRecursive(0), 1);
+}
+
+// Tests factorial of positive numbers.
+TEST(FactorialTest, HandlesPositiveInput) {
+  EXPECT_EQ(my_class_ptr->FactorialRecursive(1), 1);
+  EXPECT_EQ(my_class_ptr->FactorialRecursive(2), 2);
+  EXPECT_EQ(my_class_ptr->FactorialRecursive(3), 6);
+  EXPECT_EQ(my_class_ptr->FactorialRecursive(8), 40320);
+}
+
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+
+  my_class_ptr = std::shared_ptr<MyClass>(new MyClass());
+
+  return RUN_ALL_TESTS();
+}
